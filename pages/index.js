6c36ff4427ac656e21a8bf1b486af7e155a33ff7
nonSpecificUser7 <gsirazh@gmail.com>
2020-11-03 16:47:57 +0500

    add initial layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,107 @@
-const index = () => {
+import { Container, Row, Col, Media, Image, Card } from 'react-bootstrap';
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      <Navbar />
+      <div className='blog-detail-page'>
+        <Row>
+          <Col md="8">
+            {/* AUTHOR INTRO STARTS */}
+            <Media className="mb-4 admin-intro">
+              <Image
+                roundedCircle
+                width={64}
+                height={64}
+                className="mr-3"
+                src="https://i.insider.com/55ba87b8dd0895c81c8b4581?width=400&format=jpeg&auto=webp"
+                alt="Generic placeholder"
+              />
+              <Media.Body>
+                <h5 className="font-weight-bold mb-0">nonSpecificUser,</h5>
+                <p className="welcome-text">
+                  Hello there, it is a typical blog app.
+                </p>
+              </Media.Body>
+            </Media>
+            {/* AUTHOR INTRO ENDS */}
+          </Col>
+        </Row>
+        <hr />
+        {/* className from props */}
+        <div className={`page-wrapper`}>
+          <Row className="mb-5">
+            <Col md="10">
+              {/* CardListItem STARTS */}
+              <Card className={`fj-card fj-card-list`}>
+                <div className="card-body-wrapper">
+                  <Card.Header
+                    className="d-flex flex-row">
+                    <img
+                      src={'https://via.placeholder.com/150'}
+                      className="rounded-circle mr-3"
+                      height="50px"
+                      width="50px"
+                      alt="avatar" />
+                    <div>
+                      <Card.Title className="font-weight-bold mb-1">Placeholder Author</Card.Title>
+                      <Card.Text className="card-date">Placeholder Date</Card.Text>
+                    </div>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title className="card-main-title">Placeholder Title</Card.Title>
+                    <Card.Text>Placehodler Subtitle</Card.Text>
+                  </Card.Body>
+                </div>
+                <a href="#" className="card-button">
+                  Read More
+                </a>
+              </Card>
+              {/* CardListItem ENDS */}
+            </Col>
+
+            <Col md="4">
+              <Card className={`fj-card`}>
+                <div className="card-body-wrapper">
+                  <Card.Header
+                    className="d-flex flex-row">
+                    <img
+                      src={'https://via.placeholder.com/150'}
+                      className="rounded-circle mr-3"
+                      height="50px"
+                      width="50px"
+                      alt="avatar" />
+                    <div>
+                      <Card.Title className="font-weight-bold mb-1">Placeholder Author</Card.Title>
+                      <Card.Text className="card-date">Placeholder Date</Card.Text>
+                    </div>
+                  </Card.Header>
+                  <div className="view overlay">
+                    <Card.Img
+                      src='https://via.placeholder.com/250'
+                      alt="Card image cap"
+                    />
+                  </div>
+                  <Card.Body>
+                    <Card.Title className="card-main-title">Placeholder Title</Card.Title>
+                    <Card.Text>Placehodler Subtitle</Card.Text>
+                  </Card.Body>
+                </div>
+                <a className="card-button">
+                  Read More
+                </a>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">github</a>
+        </div>
+      </footer>
+    </Container>
   )
 }
-
-export default index
-
