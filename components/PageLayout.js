677aab30868dfe99ca78c786735d500242a77efa
nonSpecificUser7 <gsirazh@gmail.com>
2020-11-05 16:04:05 +0500

    Add google font

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import Navbar from 'components/Navbar'
 import { Container } from 'react-bootstrap'
+import Head from 'next/head'
 
 const PageLayout = ({ children, className }) => {
     return (
-        <Container>
-            <Navbar />
-
-            <div className={`page-wrapper ${className}`}>
-                {children}
-            </div>
-
-            <footer className="page-footer">
-                <div>
-                    <a href="#">github</a>
+        <>
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+            </Head>
+            <Container>
+                <Navbar />
+                <div className={`page-wrapper ${className}`}>
+                    {children}
                 </div>
-            </footer>
-        </Container>
-
+                <footer className="page-footer">
+                    <div>
+                        <a href="#">github</a>
+                    </div>
+                </footer>
+            </Container>
+        </>
     )
 }
 
