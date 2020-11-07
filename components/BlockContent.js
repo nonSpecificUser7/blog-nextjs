a21e4380bafde3056e49d73eca0ddee637f6784d
nonSpecificUser7 <gsirazh@gmail.com>
2020-11-07 19:14:41 +0500

    Add Image optimization and custom positioning

--- components/BlockContent.js
+++ components/BlockContent.js
@@ -10,9 +10,20 @@ const serializers = {
                 <div className='code-filename'>{filename}</div>
             </CodeSnippet>
         ),
-        image: ({ node: { asset, Alt } }) => {
+        image: ({ node: { asset, Alt, position = 'center' } }) => {
+            let style = {}
+            if (position === 'left') {
+                style.float = position;
+                style.marginRight = '30px';
+            }
+
+            if (position === 'right') {
+                style.float = position;
+                style.marginLeft = '30px';
+            }
+
             return (
-                <div className='blog-image'>
+                <div className='blog-image' style={{ ...style }}>
                     <img src={urlFor(asset).height(300).fit('max').url()} />
                     <div className='image-alt'>{Alt}</div>
                 </div>
