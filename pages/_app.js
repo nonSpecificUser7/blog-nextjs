c93d2ad45e80db8b162569fdfce873ea562d6c17
nonSpecificUser7 <gsirazh@gmail.com>
2020-11-09 20:08:34 +0500

    Add date order icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { faBorderAll, faList } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBorderAll,
+  faList,
+  faSortNumericDown,
+  faSortNumericUp
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBorderAll, faList)
+library.add(faList, faBorderAll, faSortNumericDown, faSortNumericUp);
 config.autoAddCss = false;
 //import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
